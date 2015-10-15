9e040cc9fde78487b597e79fbd9785b9fec6edfa
AjlaElTabari <ajla.eltabari@BitCamps-Mac-mini-65.local>
2015-10-15 13:33:27 +0200

    TODO app

--- ToDo/app/src/main/java/com/example/ajlaeltabari/todo/MainActivity.java
+++ ToDo/app/src/main/java/com/example/ajlaeltabari/todo/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.ajlaeltabari.todo;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button mAddTask;
+    private EditText mTask;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mAddTask = (Button)findViewById(R.id.btnAddTask);
+        mTask = (EditText)findViewById(R.id.txtTask);
+
+        mAddTask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LinearLayout cbsTasks = (LinearLayout) findViewById(R.id.cbsTasks);
+
+                CheckBox task = new CheckBox(MainActivity.this);
+                task.setText(mTask.getText().toString());
+                cbsTasks.addView(task);
+            }
+        });
     }
 }
---
+++ ToDo/app/src/main/java/com/example/ajlaeltabari/todo/Task.java
@@ -0,0 +1,12 @@
+package com.example.ajlaeltabari.todo;
+
+/**
+ * Created by ajla.eltabari on 15/10/15.
+ */
+public class Task {
+    private String task;
+
+    public Task(String task) {
+        this.task = task;
+    }
+}
